9b6d30dbdcbd4c7636f3ee3e63c41a5d345e5723
Kyle Geary <gearykye34@gmail.com>
2023-11-18 15:28:15 -0500

    finished

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){ 
+    return (a + b);
+};
+function increment(n){
+    n += 1;
+    return n;
+}
+function subtract(a, b){
+    
+    return a - b;
+};
+function decrement(n){
+    n -= 1;
+    return n;
+};
+function multiply(a, b){
+    return a * b;
+};
+function makeInt(n){
+    return parseInt(n,10);
+};
+function divide(a, b){
+    return a / b;
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+};
